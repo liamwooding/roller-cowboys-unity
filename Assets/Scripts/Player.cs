75c7a5d262080cbb14d92981ddbd2b7f5ef470e4
Liam Wooding <liamwooding@gmail.com>
2016-01-28 17:33:13 +0000

    Adds aiming lines

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,11 @@ public class Player : MonoBehaviour, IShootable {
   public float brakingTime = 0.2f;
   public float drag = 3f;
   public Vector3 startPosition = new Vector3(0, 1, 0);
+  public enum State {Ready, Aiming, Waiting, Moving}
+  public State state;
 
+  private LineRenderer rightHandAim;
+  private LineRenderer leftHandAim;
   private Rigidbody rb;
   private Vector3 dragStart;
   private int dragThreshold = 9;
@@ -18,22 +22,24 @@ public class Player : MonoBehaviour, IShootable {
   private Vector3 shot2;
   private float coastingTime;
   private int shotsDeclared = 0;
-  public enum State {Ready, Waiting, Moving}
-  public State state;
 
   // Use this for initialization
   void Start () {
     rb = gameObject.GetComponent<Rigidbody>();
+    leftHandAim = GameObject.Find("Left Hand Aim").GetComponent<LineRenderer>();
+    rightHandAim = GameObject.Find("Right Hand Aim").GetComponent<LineRenderer>();
   }
 
   // Update is called once per frame
   void Update () {
-    if (state == State.Ready) {
-      if (Input.GetMouseButtonDown(0)) {
-        HandleMouseDown();
-      } else if (Input.GetMouseButtonUp(0)) {
+    if (state == State.Aiming) {
+      if (Input.GetMouseButtonUp(0)) {
         HandleMouseUp();
+      } else {
+        DrawAimLine();
       }
+    } else if (state == State.Ready && Input.GetMouseButtonDown(0)) {
+      HandleMouseDown();
     } else if (state == State.Moving) {
       AdjustCoast();
     }
@@ -41,9 +47,11 @@ public class Player : MonoBehaviour, IShootable {
 
   void HandleMouseDown () {
     dragStart = Input.mousePosition;
+    state = State.Aiming;
   }
 
   void HandleMouseUp () {
+    state = State.Ready;
     var dragVector = Input.mousePosition - dragStart;
     dragVector.Normalize();
     if (Vector3.Distance(dragStart, Input.mousePosition) < dragThreshold) {
@@ -52,7 +60,6 @@ public class Player : MonoBehaviour, IShootable {
     }
 
     dragVector = Quaternion.Euler(90, 0, 0) * dragVector;
-
     DeclareShot(dragVector);
   }
 
@@ -71,6 +78,24 @@ public class Player : MonoBehaviour, IShootable {
     rb.velocity = new Vector3(0, 0, 0);
   }
 
+  void DrawAimLine () {
+    var dragVector = Quaternion.Euler(90, 0, 0) * (Input.mousePosition - dragStart);
+    LineRenderer aimLine;
+    if (shotsDeclared == 0) {
+      aimLine = leftHandAim;
+    } else {
+      aimLine = rightHandAim;
+    }
+    aimLine.SetVertexCount(2);
+    aimLine.SetPosition(0, transform.position);
+    aimLine.SetPosition(1, dragVector - transform.position);
+  }
+
+  void ClearAimLines () {
+    rightHandAim.SetVertexCount(0);
+    leftHandAim.SetVertexCount(0);
+  }
+
   void DeclareShot (Vector3 shotVector) {
     if (shotsDeclared == 0) {
       shot1 = shotVector;
@@ -82,6 +107,7 @@ public class Player : MonoBehaviour, IShootable {
   }
 
   void TakeAction () {
+    ClearAimLines();
     state = State.Moving;
     FireShots();
     coastingTime = 0f;
